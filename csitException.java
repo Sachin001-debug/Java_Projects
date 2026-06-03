7787eaebceb280c83505cd1923da91d7ead3b48a
sachinkharel444 <ukharel111@gmail.com>
2026-06-03 23:52:45 +0545

    custom exception

---
+++ csitException.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+// Custom Exception
+class CsitException extends Exception {
+    public CsitException(String message) {
+        super(message);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter faculty: ");
+        String faculty = sc.nextLine();
+
+        try {
+            if (!faculty.equalsIgnoreCase("CSIT")) {
+                throw new CsitException("You are not from CSIT faculty.");
+            }
+
+            System.out.println("You are from CSIT faculty.");
+
+        } catch (CsitException e) {
+            System.out.println(e.getMessage());
+        }
+
+        sc.close();
+    }
+}
